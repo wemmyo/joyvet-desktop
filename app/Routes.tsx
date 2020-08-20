b7692d15338d03e87735e30c5d8cc945c1ce3cae
Wemimo Oyedeji <wemmyoyedeji@gmail.com>
2020-08-20 12:19:09 +0100

    remove defalut setup

--- app/Routes.tsx
+++ app/Routes.tsx
@@ -8,22 +8,21 @@ import LoginScreen from './views/Login/Login';
 import OverviewScreen from './views/Overview/Overview';
 
 // Lazily load routes and code split with webpacck
-const LazyCounterPage = React.lazy(() =>
-  import(/* webpackChunkName: "CounterPage" */ './views/CounterPage')
-);
+// const LazyCounterPage = React.lazy(() =>
+//   import(/* webpackChunkName: "CounterPage" */ './views/CounterPage')
+// );
 
-const CounterPage = (props: Record<string, any>) => (
-  <React.Suspense fallback={<h1>Loading...</h1>}>
-    <LazyCounterPage {...props} />
-  </React.Suspense>
-);
+// const CounterPage = (props: Record<string, any>) => (
+//   <React.Suspense fallback={<h1>Loading...</h1>}>
+//     <LazyCounterPage {...props} />
+//   </React.Suspense>
+// );
 
 export default function Routes() {
   return (
     <App>
       <Switch>
         <Route path={routes.OVERVIEW} component={OverviewScreen} />
-        <Route path={routes.COUNTER} component={CounterPage} />
         <Route path={routes.LOGIN} component={LoginScreen} />
       </Switch>
     </App>
