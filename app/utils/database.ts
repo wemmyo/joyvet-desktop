03766616a0914be2a064bfbceaf00fcf27f9e158
Oyedeji David <wemmyoyedeji@gmail.com>
2022-02-04 23:05:37 +0000

    refactor database config

--- app/utils/database.ts
+++ app/utils/database.ts
@@ -6,85 +6,79 @@ const { app } = require('electron').remote;
 
 const Sequelize = require('sequelize');
 
+export const openDialog = () => {
+  const databasePath = remote.dialog.showOpenDialogSync({
+    properties: ['openFile'],
+    filters: [{ name: 'Datbase', extensions: ['db', 'sqlite', 'sql'] }],
+  });
+  if (databasePath) {
+    return databasePath[0];
+  }
+  return databasePath;
+};
+
 const checkForDB = () => {
+  /*
+  userData The directory for storing your app's configuration files, 
+  which by default it is the appData directory appended with your app's name.
+  */
   const userDataDir = app.getPath('userData');
   const absolutePath = path.join(userDataDir, 'pathToDB');
 
+  // if the database path is stored in app data, read the file path
   if (fs.existsSync(absolutePath)) {
     return fs.readFileSync(absolutePath, 'utf8');
   }
+  // Else open save dialog
   const pathContent = remote.dialog.showSaveDialogSync({
     title: 'Select folder',
     defaultPath: 'joyvet.db',
     properties: ['createDirectory'],
   });
+
+  // Close app if path isn't given or cancel button pressed
+  if (!pathContent) {
+    app.quit();
+    return null;
+  }
+
   fs.writeFileSync(absolutePath, pathContent);
   return fs.readFileSync(absolutePath, 'utf8');
 };
 
-// export default new Sequelize({
-//   dialect: 'sqlite',
-//   storage: checkForDB(),
-// });
-
-let database;
-
-const development = {
-  database: 'joyvetStaging',
-  username: 'root',
-  password: '',
-  host: 'localhost',
-  dialect: 'mysql',
+const checkForTestDB = () => {
+  const testDatabasePath = openDialog();
+  if (!testDatabasePath) {
+    app.quit();
+  }
+  return testDatabasePath;
 };
+
+let database: any;
 
 const production = {
   dialect: 'sqlite',
   storage: checkForDB(),
 };
 
-// export default new Sequelize('joyvetStaging', 'root', '', {
-//   dialect: 'mysql',
-//   host: 'localhost',
-// });
-
 switch (process.env.NODE_ENV) {
   case 'production':
     database = new Sequelize({
       dialect: production.dialect,
-      storage: checkForDB(),
+      storage: checkForTestDB(),
     });
     break;
-  case 'development':
-    database = new Sequelize(
-      development.database,
-      development.username,
-      development.password,
-      {
-        host: development.host,
-        dialect: development.dialect,
-        pool: {
-          max: 5,
-          min: 0,
-          idle: 10000,
-        },
-      }
-    );
-    break;
+  // case 'development':
+  //   database = new Sequelize({
+  //     dialect: production.dialect,
+  //     storage: checkForTestDB(),
+  //   });
+  //   break;
   default:
-    database = new Sequelize(
-      development.database,
-      development.username,
-      development.password,
-      {
-        host: development.host,
-        dialect: development.dialect,
-        pool: {
-          max: 5,
-          min: 0,
-          idle: 10000,
-        },
-      }
-    );
+    database = new Sequelize({
+      dialect: production.dialect,
+      storage: checkForDB(),
+    });
 }
 
 export default database;
