6ae1461ee3a0b9b54bec581278c4249dc67b348a
David Oyedeji <david.oyedeji@prettylittlething.com>
2023-12-28 19:17:54 +0000

    Update dependencies in package.json

--- app/main.dev.ts
+++ app/main.dev.ts
@@ -66,9 +66,11 @@ const createWindow = async () => {
       process.env.ERB_SECURE !== 'true'
         ? {
             nodeIntegration: true,
+            enableRemoteModule: true,
           }
         : {
             preload: path.join(__dirname, 'dist/renderer.prod.js'),
+            enableRemoteModule: true,
           },
   });
 
